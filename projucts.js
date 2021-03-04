b15316e095e555845ee3659f8490c10c70b6b336
Anuoluwapo Abolarin <37321402+Anu-oluwapo@users.noreply.github.com>
2021-03-04 06:25:27 +0100

    Add files via upload

---
+++ projucts.js
@@ -0,0 +1,52 @@
+let productArray = [
+
+    {
+        id: 1,
+        title: "cat food 3 bags",
+        decsription: "its just <em> irresistable </em> cat food tbh my guy" ,
+        price : 2300,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB2ysrPvhIBLoo22vh1IW1cPRT4YEaSA52Rg&usqp=CAU",
+        availableInventory: 10,
+        rating : 2,
+    },
+
+    {
+        id: 2,
+        title: "Dog food 3 bags",
+        decsription: "its just <em> irresistable </em> cat food tbh my guy" ,
+        price : 1200,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB2ysrPvhIBLoo22vh1IW1cPRT4YEaSA52Rg&usqp=CAU",
+        availableInventory: 10,
+        rating : 2,
+    },
+
+    {
+        id: 3,
+        title: "Horse food 3 bags",
+        decsription: "its just <em> irresistable </em> cat food tbh my guy" ,
+        price : 280,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB2ysrPvhIBLoo22vh1IW1cPRT4YEaSA52Rg&usqp=CAU",
+        availableInventory: 10,
+        rating : 2,
+    },
+
+    {
+        id: 4,
+        title: "Bird food 3 bags",
+        decsription: "its just <em> irresistable </em> cat food tbh my guy" ,
+        price : 500,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB2ysrPvhIBLoo22vh1IW1cPRT4YEaSA52Rg&usqp=CAU",
+        availableInventory: 10,
+        rating : 2,
+    },
+
+    {
+        id: 5,
+        title: "Posey food 3 bags",
+        decsription: "its just <em> irresistable </em> cat food tbh my guy" ,
+        price : 20,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB2ysrPvhIBLoo22vh1IW1cPRT4YEaSA52Rg&usqp=CAU",
+        availableInventory: 10,
+        rating : 1,
+    },
+];
