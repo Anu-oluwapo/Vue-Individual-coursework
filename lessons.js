967ffbfa79b2c579a7bcaac8c4e08f199fd4ccbb
anu-oluwapo <abolarin.aanu@gmail.com>
2021-03-04 11:14:18 +0100

    Created lesson array and fixed some bugs

---
+++ lessons.js
@@ -0,0 +1,102 @@
+let lessons = [
+
+    {
+        id: 1,
+        subject: "Math",
+        location: "London" ,
+        price : 300,
+        image: "https://i.pinimg.com/originals/c8/e5/75/c8e5753370bad54c7977d485e0a0e29d.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "English",
+        location: "South Hampton" ,
+        price : 100,
+        image: "https://lawctopus.com/clatalogue/wp-content/uploads/2020/11/Prefix-and-Suffix.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Music",
+        location: "Germany" ,
+        price : 300,
+        image: "https://cdn.vox-cdn.com/thumbor/0fPNabFt96yEmvSfyr7cHHZdjbw=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13292797/acastro_181016_1777_music_0001.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Geography",
+        location: "London" ,
+        price : 300,
+        image: "https://previews.123rf.com/images/edwardsamuel/edwardsamuel1607/edwardsamuel160700074/60411488-geography-sign-banner-word-for-geography-schools-education-subject-teaching-isolated-on-white-backgr.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "History",
+        location: "London" ,
+        price : 300,
+        image: "https://thumbs.dreamstime.com/b/history-doodles-lettering-white-background-137829155.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Theology",
+        location: "London" ,
+        price : 300,
+        image: "https://journals.sagepub.com/pb-assets/cover-alt/tjx-cover-social-1565280565687.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Social Studies",
+        location: "London" ,
+        price : 300,
+        image: "https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2015/01/SocialStudiesWordle2.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Health Education",
+        location: "London" ,
+        price : 300,
+        image: "https://seofiles.s3.amazonaws.com/seo/media/cache/fe/e7/fee79bafac7d456a9fd1f7556ab38029.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Business Management",
+        location: "London" ,
+        price : 300,
+        image: "https://vermontrepublic.org/wp-content/uploads/2019/01/Business-Management-Facts-1.jpg",
+        availablespace: 5,
+        rating : 2,
+    },
+
+    {
+        id: 1,
+        subject: "Information Technology",
+        location: "London" ,
+        price : 300,
+        image: "https://media-exp1.licdn.com/dms/image/C511BAQE0cVYmg8QN0g/company-background_10000/0/1581481407499?e=2159024400&v=beta&t=tp4fzua9GkgJLUYXf0JEMnIBFCM-wlKKDnIuiqfnJbc",
+        availablespace: 5,
+        rating : 2,
+    },
+];
