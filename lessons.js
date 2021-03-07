4fa844f35f68a8d87b96a8953784a88a58af582a
anu-oluwapo <abolarin.aanu@gmail.com>
2021-03-07 13:40:38 +0100

    Fixed The dreadful sort Methids that were'nt working, took me almost 3 whole days

--- lessons.js
+++ lessons.js
@@ -8,18 +8,20 @@ let lessonsArray = [
         image: "https://i.pinimg.com/originals/c8/e5/75/c8e5753370bad54c7977d485e0a0e29d.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 2,
         subject: "English",
         location: "South Hampton" ,
         price : 100,
         image: "https://lawctopus.com/clatalogue/wp-content/uploads/2020/11/Prefix-and-Suffix.jpg",
-        availablespace: 5,
+        availablespace: 2,
         rating : 2,
+        
     },
-
+    
     {
         id: 3,
         subject: "Music",
@@ -28,8 +30,9 @@ let lessonsArray = [
         image: "https://cdn.vox-cdn.com/thumbor/0fPNabFt96yEmvSfyr7cHHZdjbw=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13292797/acastro_181016_1777_music_0001.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 4,
         subject: "Geography",
@@ -38,8 +41,9 @@ let lessonsArray = [
         image: "https://previews.123rf.com/images/edwardsamuel/edwardsamuel1607/edwardsamuel160700074/60411488-geography-sign-banner-word-for-geography-schools-education-subject-teaching-isolated-on-white-backgr.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 5,
         subject: "History",
@@ -48,8 +52,9 @@ let lessonsArray = [
         image: "https://thumbs.dreamstime.com/b/history-doodles-lettering-white-background-137829155.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 6,
         subject: "Theology",
@@ -58,8 +63,9 @@ let lessonsArray = [
         image: "https://journals.sagepub.com/pb-assets/cover-alt/tjx-cover-social-1565280565687.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 7,
         subject: "Social Studies",
@@ -68,8 +74,9 @@ let lessonsArray = [
         image: "https://uconn-today-universityofconn.netdna-ssl.com/wp-content/uploads/2015/01/SocialStudiesWordle2.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 8,
         subject: "Health Education",
@@ -78,8 +85,9 @@ let lessonsArray = [
         image: "https://seofiles.s3.amazonaws.com/seo/media/cache/fe/e7/fee79bafac7d456a9fd1f7556ab38029.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 9,
         subject: "Business Management",
@@ -88,8 +96,9 @@ let lessonsArray = [
         image: "https://vermontrepublic.org/wp-content/uploads/2019/01/Business-Management-Facts-1.jpg",
         availablespace: 5,
         rating : 2,
+        
     },
-
+    
     {
         id: 10,
         subject: "Information Technology",
@@ -98,5 +107,6 @@ let lessonsArray = [
         image: "https://media-exp1.licdn.com/dms/image/C511BAQE0cVYmg8QN0g/company-background_10000/0/1581481407499?e=2159024400&v=beta&t=tp4fzua9GkgJLUYXf0JEMnIBFCM-wlKKDnIuiqfnJbc",
         availablespace: 5,
         rating : 2,
+        
     },
-];
+    ];
